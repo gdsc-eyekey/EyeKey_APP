77671b4e40e5cba0974a03abcf704a0c7047da6e
zinhyeok <david9pjh@gmail.com>
2023-02-28 10:43:13 +0900

    FEAT:[gallery 기능 추가 & permission 2개설정]

--- app/src/main/java/com/gdsc/eyekey/MainActivity.kt
+++ app/src/main/java/com/gdsc/eyekey/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gdsc.eyekey
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -22,77 +23,103 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     private var imageView:ImageView? = null
 
-    private val CAMERA_PERMISSION = arrayOf(android.Manifest.permission.CAMERA)
-    private val STORAGE_PERMISSION = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
-        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result ->
+            if(result.resultCode == RESULT_OK && result.data!=null){
+                val imageBackGround: ImageView = findViewById(R.id.imagePreView)
+                imageBackGround.setImageURI(result.data?.data)
+            }
+        }
 
-    // using at startActivityForResult
-    private  val PERMISSON_CAMERA = 1
-    private  val PERMISSON_STORAGE = 2
-    private  val REQUEST_CAMERA = 3
-    private  val REQUEST_STORAGE = 4
+
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+                if(isGranted && permissionName == android.Manifest.permission.READ_EXTERNAL_STORAGE){
+                    Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_LONG).show()
+                    //url 받아서 image 넘기기
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                }
+                if(isGranted && permissionName == android.Manifest.permission.CAMERA){
+                    Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_LONG).show()
+                    //Todo Camera 어플 실행
+                }
+                else{
+                    if(permissionName == android.Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(
+                            this@MainActivity, "Permission denied can't use external storgae",
+                            Toast.LENGTH_LONG).show()
+                    }
+                    if(permissionName == android.Manifest.permission.CAMERA){
+                        Toast.makeText(
+                            this@MainActivity, "Permission denied can't use camera",
+                            Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         imageView = findViewById(R.id.imagePreView)
+
         val ibGallery : ImageButton = findViewById(R.id.ib_gallery)
         ibGallery.setOnClickListener{
-            openGallery()
+            requestStoragePermission()
         }
 
         val ibCamera : ImageButton = findViewById(R.id.ib_camera)
         ibCamera.setOnClickListener{
-            openCamera()
+            requestCameraPermission()
         }
 
     }
 
-    private fun checkPermission(permissons: Array<out String>, flag: Int):Boolean{
-        for(permisson in permissons){
-            if(ContextCompat.checkSelfPermission(this, permisson) !=
-                PackageManager.PERMISSION_GRANTED){
-                    androidx.core.app.ActivityCompat.requestPermissions(this, permissons, flag)
-                return false
+    private fun isReadStorageAllowed(): Boolean{
+        val result = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+            showRationalDialog("EyeKey App", "This App " +
+                    "needs to Access your External storage for getting image")
+        }else{
+            requestPermission.launch(arrayOf(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ))
+        }
+    }
+    private fun requestCameraPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+            showRationalDialog("EyeKey App", "This App " +
+                    "needs to Access your Camera for getting image")
+        }else{
+            requestPermission.launch(arrayOf(
+                android.Manifest.permission.CAMERA
+            ))
+        }
+    }
+    private fun showRationalDialog(
+        title: String, message: String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancle"){
+                    dialog, _ -> dialog.dismiss()
             }
-        }
-        return true
+        builder.create().show()
     }
-
-    private fun openCamera(){
-        if(checkPermission(CAMERA_PERMISSION, PERMISSON_CAMERA)){
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(intent, REQUEST_CAMERA)
-        }
-    }
-    private fun openGallery(){
-        if(checkPermission(STORAGE_PERMISSION, PERMISSON_STORAGE)){
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = MediaStore.Images.Media.CONTENT_TYPE
-            startActivityForResult(intent, REQUEST_STORAGE)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            when(requestCode){
-                REQUEST_CAMERA -> {
-                    val bitmap = data?.extras?.get("data") as Bitmap
-                    imageView?.setImageBitmap(bitmap)
-                    // TODO: 갤러리에 사진 저장 기능 넣기
-                }
-                REQUEST_STORAGE -> {
-                    val uri = data?.data
-                    imageView?.setImageURI(uri)
-                }
-            }
-
-        }
-    }
-
-
 
 
 
